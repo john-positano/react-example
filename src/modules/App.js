3f2788c6b7634256b9161dd625c3ee4437a0c5ab
John Positano <johnmtpositano@gmail.com>
2024-02-01 22:51:37 -0500

    first shot

---
+++ src/modules/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import TopBar from './TopBar'
+
+function App() {
+  return (
+    <div className="Container">
+      <TopBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/TopBar/index.js
@@ -0,0 +1,5 @@
+import './index.scss'
+
+export default () => <div className="TopBar">
+  <div className="Title">Tyliqq</div>
+</div>
